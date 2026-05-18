f31cd5f297a816165a1123d11b49eb5ace089e3b
RishavLC <shrestharishav444@gmail.com>
2026-05-18 11:56:38 +0545

    added file to github

---
+++ ReactPlayground/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+import TravelJournal from './pages/TravelJournal'
+function App() {
+
+  return (
+    <>
+      <TravelJournal />
+    </>
+  )
+}
+
+export default App
---
+++ ReactPlayground/src/pages/TravelJournal.jsx
@@ -0,0 +1,10 @@
+import Header from '../components/Travel/Header';
+import Entry from '../components/Travel/Entry';
+export default function TravelJournal(){
+    return(
+        <>
+            <Header />
+            <Entry />
+        </>
+    );
+}
